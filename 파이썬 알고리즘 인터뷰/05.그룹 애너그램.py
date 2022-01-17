722f1dc1cf76b0cd9a469cfd9c4d5244be35c2ce
DESKTOP-C7RFSO4\jbc <phobas88@gmail.com>
2022-01-17 17:33:54 +0900

    애너그램(defaultdict(list), ''.join)

---
+++ 파이썬 알고리즘 인터뷰/05.그룹 애너그램.py
@@ -0,0 +1,16 @@
+"""
+문자열 배열을 받아 애너그램 단위로 그룹핑하시오
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+"""
+from typing import List
+import collections
+
+def anagrams (group:List[str]) -> None:
+   anagrams = collections.defaultdict(list)
+
+   for word in group:
+       anagrams[''.join(sorted(word))].append(word)
+   return list(anagrams.values())
+
+anagrams(["eat","tea","tan","ate","nat","bat"])
